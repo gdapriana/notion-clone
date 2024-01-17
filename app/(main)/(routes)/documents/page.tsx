034eee12f8b92a273f2b2ef9e48435bc973f0442
gdapriana <gedeapriana36@gmail.com>
2024-01-17 13:52:25 +0800

    ⚙ user setting️

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -1,5 +1,28 @@
+'use client';
+
+import Image from 'next/image';
+import { useUser } from '@clerk/clerk-react';
+
 const DocumentsPage = () => {
-  return <div>Document</div>;
+  const { user } = useUser();
+  return (
+    <div className="h-full flex flex-col items-center justify-center space-y-4">
+      <Image
+        src="/empty.png"
+        alt="Empty"
+        height="300"
+        width="300"
+        className="dark:hidden"
+      />
+      <Image
+        src="/empty-dark.png"
+        alt="Empty"
+        height="300"
+        width="300"
+        className="hidden dark:block"
+      />
+    </div>
+  );
 };
 
 export default DocumentsPage;
